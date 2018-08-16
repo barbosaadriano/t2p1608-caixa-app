c27726099bebf32cd961c11894b9287cc2aad194
Adriano Barbosa <b.adrianobarbosa@gmail.com>
2018-08-16 20:37:29 -0300

    implementações da primeira aula

--- Caixa/src/br/com/adrianob/dao/CaixaDao.java
+++ Caixa/src/br/com/adrianob/dao/CaixaDao.java
@@ -3,9 +3,14 @@ package br.com.adrianob.dao;
 
 import br.com.adrianob.model.Caixa;
 import com.mysql.jdbc.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -29,9 +34,29 @@ public class CaixaDao {
         
     }
     public ArrayList<Caixa> getAll() {
-        String sql = "";
-        
-        return null;
+        ArrayList<Caixa> lista = new ArrayList<>();
+        String sql = "select * from tbl_caixa ";
+        try {
+            PreparedStatement ps 
+                    = this.cnx.prepareStatement(sql);
+            ResultSet res = ps.executeQuery();
+            while (res.next()) {
+                Caixa c = new Caixa();
+                c.setData(res.getDate("data"));
+                c.setSaldoInicial(res.getDouble("saldoinicial"));
+                c.setEntradas(res.getDouble("entradas"));
+                c.setSaidas(res.getDouble("saidas"));
+                c.setSaldoFinal(res.getDouble("saldofinal"));
+                String tmp = res.getString("status");
+                if (tmp.equals("Fechado")) {
+                    c.setStatus(Caixa.StatusCaixa.FECHADO);
+                }
+                lista.add(c);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return lista;
     }
     
     public Caixa getCaixa(Date data) {
