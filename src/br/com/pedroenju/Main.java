ee59185c54c757729aaaf1b6ca62fffadef7d5b0
Pedro Enju <pedro_enju@hotmail.com>
2018-08-16 22:38:53 -0300

    Foi

--- src/br/com/pedroenju/Main.java
+++ src/br/com/pedroenju/Main.java
@@ -1,9 +1,46 @@
 package br.com.pedroenju;
 
+import br.com.pedroenju.dao.CaixaDao;
+import br.com.pedroenju.model.Caixa;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Date;
+import java.util.Properties;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Properties config = new Properties();
+        config.put("user", "root");
+        config.put("password", "enju");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn
+                    = DriverManager.getConnection(
+                            "jdbc:mysql://localhost:3306/singleton",
+                            config
+                    );
+            CaixaDao cd = new CaixaDao((com.mysql.jdbc.Connection) conn);
+            cd.createtable();
+            
+            Caixa c = new Caixa();
+            c.setSaldoInicial(100);
+            
+            cd.salvar(c);
+            
+            c.setEntradas(50);
+            
+            cd.salvar(c);
+            
+            Caixa caixa = cd.getCaixa(new Date());
+            
+            System.out.println(caixa.getData());
+            System.out.println(caixa.getSaldoInicial());
+            System.out.println(caixa.getStatus());
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
--- src/br/com/pedroenju/dao/CaixaDao.java
+++ src/br/com/pedroenju/dao/CaixaDao.java
@@ -23,16 +23,49 @@ public class CaixaDao {
 
     public void salvar(Caixa c) { //Insert e Update
 
+        Caixa caixa = this.getCaixa(c.getData());
+
+        String sql = "update tbl_caixa set saldoinicial = ?,entradas = ?,"
+                + "saidas = ?,saldofinal = ?,status = ? where data = ?";
+
+        if (caixa == null) {
+            sql = "insert into tbl_caixa"
+                    + " (saldoinicial,entradas,saidas,saldofinal,status,data)"
+                    + " values "
+                    + " (?,?,?,?,?,?)";
+        }
+        try {
+            PreparedStatement ps = this.cnx.prepareStatement(sql);
+            ps.setDouble(1, c.getSaldoInicial());
+            ps.setDouble(2, c.getEntradas());
+            ps.setDouble(3, c.getSaidas());
+            ps.setDouble(4, c.getSaldoFinal());
+            ps.setString(5, c.getStatus().name());
+            ps.setString(6, this.sdf.format(c.getData()));
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 
     public void remover(Caixa c) { //Delete
 
+        String sql = "delete from tbl_caixa where data = ?";
+
+        try {
+            PreparedStatement ps = this.cnx.prepareStatement(sql);
+            ps.setString(1, sdf.format(c.getData()));
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
-    public ArrayList<Caixa> getAll() { //Select
+    public ArrayList<Caixa> getAll(String where) { //Select
         ArrayList<Caixa> lista = new ArrayList<>();
 
-        String sql = "select * from tbl_caixa ";
+        String sql = "select * from tbl_caixa" + where;
 
         try {
             PreparedStatement ps = this.cnx.prepareStatement(sql);
@@ -58,8 +91,31 @@ public class CaixaDao {
 
     public Caixa getCaixa(Date data) {
 
+        String dt = this.sdf.format(data);
+        ArrayList<Caixa> all = this.getAll(" where data = '" + dt + "'");
+        if (!all.isEmpty()) {
+            return all.get(0);
+        }
         return null;
+    }
 
+    public void createtable() {
+
+        String sql = "create table if not exists tbl_caixa ("
+                + " data date primary key,"
+                + " saldoinicial numeric(13,2),"
+                + " entradas numeric(13,2),"
+                + " saidas numeric(13,2),"
+                + " saldofinal numeric(13,2),"
+                + " status varchar(7) )";
+        
+        try {
+            PreparedStatement ps = this.cnx.prepareStatement(sql);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
     }
 
 }
